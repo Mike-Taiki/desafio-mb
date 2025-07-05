ee170b34d485aee1d08889848c5ed01b527225ba
michael <michael09gomes@gmail.com>
2025-07-05 19:51:43 -0300

    feat: add step component

--- client/src/modules/register/Register.vue
+++ client/src/modules/register/Register.vue
@@ -1,13 +1,17 @@
 <script setup>
 import { ref } from "vue";
+import { headingOptionsEnum } from "@/shared/components/Heading/types.js";
 import Heading from "@/shared/components/Heading/Heading.vue";
 import Input from "@/shared/components/Input/Input.vue";
-import { headingOptionsEnum } from "@/shared/components/Heading/types.js";
+import Step from "@/shared/components/Step/Step.vue";
 
 const name = ref("");
+const currentStep = ref(1);
+const totalSteps = ref(5);
 </script>
 
 <template>
+  <Step :current-step="currentStep" :total-steps="totalSteps" />
   <Heading :level="headingOptionsEnum.MD">Seja bem vindo(a)</Heading>
-  <Input v-model="name" label="Endereço de e-mail" id="email" />
+  <Input label="Endereço de e-mail" id="email" v-model="name" />
 </template>
---
+++ client/src/shared/components/Step/Step.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+  currentStep: {
+    type: Number,
+    default: 1,
+  },
+  totalSteps: {
+    type: Number,
+    default: 5,
+  },
+});
+</script>
+<template>
+  <span class="step"
+    >Etapa <span class="step__current">{{ props.currentStep }}</span> de
+    {{ props.totalSteps }}
+  </span>
+</template>
+<style lang="scss" scoped>
+.step {
+  &__current {
+    color: $text-orange;
+  }
+}
+</style>
