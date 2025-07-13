0015d2af15aa518933f1351e66638011a8b0a02f
Michael gomes <michael.ferreira@poatek.com>
2025-07-13 18:55:49 -0300

    fix: password validation

--- server/domain/registration/validations/person.js
+++ server/domain/registration/validations/person.js
@@ -37,8 +37,8 @@ class Person {
 			throw new Error("Password is required");
 		}
 
-		if (password.length < 8 || password.length > 16) {
-			throw new Error("Password must be between 8 and 16 characters");
+		if (password.length !== 6) {
+			throw new Error("Password must be 6 characters");
 		}
 	}
 }
