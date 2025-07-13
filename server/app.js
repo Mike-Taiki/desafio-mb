7612298d4822ab61530493fadf3a6d4cce5c693e
Michael gomes <michael.ferreira@poatek.com>
2025-07-13 18:26:46 -0300

    refactor: remove unecessary code

--- server/app.js
+++ server/app.js
@@ -1,24 +1,9 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 const port = 3000;
 const registration = require("./controllers/registration");
 
 app.use(express.json());
-
-const options = {
-	dotfiles: "ignore",
-	etag: false,
-	extensions: ["htm", "html"],
-	index: false,
-	maxAge: "1d",
-	redirect: false,
-	setHeaders(res, path, stat) {
-		res.set("x-timestamp", Date.now());
-	},
-};
-
-app.use(express.static("/dist", options));
 
 app.use("/registration", registration);
 
